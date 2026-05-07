1853901163a81beb2e479a88ab1b3d58cc619ffa
Chetan Pratap Singh <chetan@Chetans-MacBook-Air.local>
2026-05-07 17:45:55 +0530

    fix: force dynamic for searchParams

--- app/consultation/page.tsx
+++ app/consultation/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+export const dynamic = "force-dynamic";
 
 import { useSearchParams } from 'next/navigation'
 import { Phone, CheckCircle2, ShieldCheck, Truck, Users, Star } from "lucide-react"
